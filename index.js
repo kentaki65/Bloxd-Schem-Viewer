0c2bc86b1a097e5f1ccbcd38cb9f22edccce886d
kentaki <kentaki@users.noreply.github.com>
2026-03-08 22:09:46 +0900

    Update: hello world :D

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello Render!");
+  res.send("Hello World!");
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

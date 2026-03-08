49b57064febdf58dea99b030132718dad3bcbb18
kentaki <kentaki@users.noreply.github.com>
2026-03-09 00:33:40 +0900

    fixed index.js

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
 app.use(express.static("public"));
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

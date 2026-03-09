bdc761b884a0481911908845ea8ecf0ee212940c
kentaki <kentaki@users.noreply.github.com>
2026-03-10 00:44:12 +0900

    update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const multer = require("multer");
-const bloxd = require("./bloxd.js");
+const bloxd = require("./parser.js");
 
 const app = express();
 const upload = multer({ storage: multer.memoryStorage() });

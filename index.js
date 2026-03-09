e1907684c3184dcf9472cc4cdfe6e740ffe39c77
kentaki <kentaki@users.noreply.github.com>
2026-03-10 00:04:39 +0900

    Added file upload function

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const multer = require("multer");
+
 const app = express();
+const upload = multer({ storage: multer.memoryStorage() });
+
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
-
+app.post("/upload", upload.single("schem"), (req, res) => {
+  const buffer = req.file.buffer;
+  console.log("received file: ", buffer.length);
+})
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ public/main.js
@@ -0,0 +1,18 @@
+const input = document.getElementById("fileInput");
+const attached = document.getElementById("attached");
+attached.onclick = () => input.click();
+
+input.addEventListener("change", async (event) => {
+  const file = event.target.files[0];
+  if(!file) return;
+
+  const form = new FormData();
+  form.append("schem", file);
+  const res = await fetch("/upload", {
+    method: "POST",
+    body: form,
+  });
+
+  const json = await res.json();
+  console.log(json);
+});

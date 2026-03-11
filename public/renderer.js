ffed2a91c18304a2730874b217afe6dbc25e0c1a
kentaki <kentaki@users.noreply.github.com>
2026-03-11 14:50:55 +0900

    update

--- public/renderer.js
+++ public/renderer.js
@@ -1,7 +1,9 @@
 // renderer.js
 import * as THREE from "https://unpkg.com/three@0.160.0/build/three.module.js";
+import { OrbitControls } from "https://unpkg.com/three@0.160.0/examples/jsm/controls/OrbitControls.js";
 
 const scene = new THREE.Scene();
+
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
@@ -10,13 +12,18 @@ const camera = new THREE.PerspectiveCamera(
 );
 
 camera.position.set(40,40,40);
-camera.lookAt(0,0,0);
 
 const renderer = new THREE.WebGLRenderer();
 const viewer = document.getElementById("view");
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 viewer.appendChild(renderer.domElement);
+
+
+// マウス操作
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.target.set(0,0,0);
+controls.update();
 
 
 // ライト
@@ -35,21 +42,27 @@ const material = new THREE.MeshStandardMaterial({
   color: 0xcccccc
 });
 
+
 export function draw(blocks){
   for (const b of blocks) {
+
     const cube = new THREE.Mesh(geometry, material);
+
     cube.position.set(
       b.x,
       b.y,
       b.z
     );
+
     scene.add(cube);
   }
 }
 
+
 // render loop
 function animate(){
   requestAnimationFrame(animate);
+  controls.update(); // ←これ重要
   renderer.render(scene, camera);
 }
 
